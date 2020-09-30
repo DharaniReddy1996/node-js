09b8dd2a8a4ce5a4d8671f9e7c60faa805ef36d9
Dharani Ganapam <dganapa1@in.ibm.com>
2020-09-30 16:59:38 +0530

    os methods added

--- nodeos.js
+++ nodeos.js
@@ -1,3 +1,4 @@
+const { get } = require('http');
 const os = require('os');
 let release=os.release();
 let platform=os.platform();
@@ -5,6 +6,10 @@ let arc=os.arch();
 let hostName=os.hostname();
 let freeMem=os.freemem();
 let homeDir=os.homedir()
+let cpus=os.cpus();
+let loadavg=os.loadavg();
+let getPriority=os.getPriority();
 let networkInterfaces=os.networkInterfaces();
-console.log(`${release} ${platform} ${arc} ${hostName} ${freeMem} ${homeDir} ${networkInterfaces}`);
+console.log(`${release} ${platform} ${arc} ${hostName} ${freeMem} ${homeDir} ${networkInterfaces}
+${cpus} ${loadavg} ${getPriority}` );
 

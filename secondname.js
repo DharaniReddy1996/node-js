2d4153c498f3d03994960d3ca8130189bb9c8a17
Dharani Ganapam <dganapa1@in.ibm.com>
2020-09-30 13:18:48 +0530

    concate-try-catch-exceptions

--- secondname.js
+++ secondname.js
@@ -1,10 +1,16 @@
 function returnsecondname(sname)
 {
-    return "your last name is:" +sname;
-    if(sname=="")
-    {
-        
-    }
+    // return "your last name is:" +sname;
+    try {
+
+        if (sname==(isNaN) && sname=="null")
+            throw err1;
+            
+        return 'your first name is: '+sName;
+        }
+         catch(err){
+            return 'Please enter the valid name';
+                }
 }
 
 module.exports.secondname=returnsecondname;

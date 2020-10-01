8515b3136dfbbe39d77ee198b291135d124676f2
Dharani Ganapam <dganapa1@in.ibm.com>
2020-10-01 16:07:17 +0530

    create-servr-http2

---
+++ create_server_http2.js
@@ -0,0 +1,10 @@
+const { captureRejectionSymbol } = require('events');
+const http =require('http');
+const server = http.createServer(((req,res)=>{
+    console.log(req.url);
+    console.log(req.headers);
+    console.log(req.method)
+})
+);
+server.listen(3000);
+console.log("server  started on port 3000")

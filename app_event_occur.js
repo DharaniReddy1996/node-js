d7098c7588b3a52fc1ac4115faa703118e241867
Dharani Ganapam <dganapa1@in.ibm.com>
2020-10-01 11:01:22 +0530

    event occur

---
+++ app_event_occur.js
@@ -0,0 +1,15 @@
+const EventEmitter = require('events');
+const Logger=require('./event_occur');
+const logger=new Logger();
+logger.log('calling log method from logger module');
+logger.log1('calling log1 method from logger module');
+logger.addListener('messageLogged',()=>{
+    console.log('Handled messageLogged Event');
+});
+logger.addListener('messageLogged',()=>
+{
+   console.log('Handled messageLogged1 Event');
+});
+logger.log('=======END=======');
+
+// by using inherit we can handel the other module register events

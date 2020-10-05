e81d63cf4491d723a695c5f00a51883084d6bbb8
Dharani Ganapam <dganapa1@in.ibm.com>
2020-10-05 12:38:22 +0530

    form-demo

--- node-routing-demo/index.js
+++ node-routing-demo/index.js
@@ -25,5 +25,6 @@ const server=http.createServer(function(req,res){
     }
 
 });
-const port =  3000;
+// const port =  3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => console.log(`Listening on port ${port}...`));

8af81b29ddce410890665b095754287bff4122b6
Dharani Ganapam <dganapa1@in.ibm.com>
2020-10-08 12:52:51 +0530

    find using first name

--- mongodb-compass/index.js
+++ mongodb-compass/index.js
@@ -41,26 +41,62 @@ const Student=mongoose.model('Student',studentSchema);
 
 // to retrive the total students list in database use getstudent() and student.find
 
-async function getStudents()
+// async function getStudents()
+// {
+//     let result = await Student.find();
+//     console.log('displaying students',result);
+//     console.log('called getStudents()');
+// }
+
+// async function run()
+// {
+//     getStudents().then(function(){
+//         console.log('called run function');
+//     }).catch(error => {
+//         console.log(error.message);
+//     })
+// }
+// run().then(function () {
+//     console.log('finished running');
+
+// }).catch(error => {
+//     console.log(error.message);
+
+// });
+
+
+// finding student with id
+
+// async function findStudentByID(id)
+// {
+//     await Student.findById(id).then(function (result) {
+//         console.log('Student found:',result);
+
+//     }).catch(error => {
+//         console.log('Error finding student with ID: ',id);
+//     });
+// }
+
+// findStudentByID('5f7ea3d6a6d759357cc86750').then(()=>{
+//     console.log('finished finding student')
+
+// }).catch(reason => {
+//     console.log('Error with finding student')
+// });
+
+// find bt first name
+
+async function findStudentByNAME(name)
 {
-    let result = await Student.find();
-    console.log('displaying students',result);
-    console.log('called getStudents()');
+    let result = await Student.find({firstName: name})
+    .select("firstName lastName")
+        console.log('Student found:',result);
+
 }
 
-async function run()
-{
-    getStudents().then(function(){
-        console.log('called run function');
-    }).catch(error => {
-        console.log(error.message);
-    })
-}
-run().then(function () {
-    console.log('finished running');
+findStudentByNAME('dharani').then(()=>{
+    console.log('finished finding student')
 
-}).catch(error => {
-    console.log(error.message);
-
-});
-
+}).catch(reason => {
+    console.log('Error with finding student')
+}); 

3c738095a47c7558573b3f5694d01128286b5554
Dharani Ganapam <dganapa1@in.ibm.com>
2020-10-09 11:49:28 +0530

    update first using update and updateMany method

--- mongodb-courses-find-09-10/app.js
+++ mongodb-courses-find-09-10/app.js
@@ -15,25 +15,44 @@ const courseSchema = new mongoose.Schema({
 });
 const Course = mongoose.model('Course', courseSchema);
 
-async function updateCourse(id)
-{
+// async function updateCourse(id)
+// {
 
-    const course = await Course.findById(id);
-    console.log(course);
-    if (!course) return;
+//     const course = await Course.findById(id);
+//     console.log(course);
+//     if (!course) return;
 
       // if set not work then use course.name stsmt
     // course.set({ name: 'Learning React',author: 'Mosh',_v: 2});
   
 
-    course.name='Spring', course.author='Jane';
+    // course.name='Spring', course.author='Jane';
 
-    course.save();
-    console.log(course);
+//     course.save();
+//     console.log(course);
 
 
+// }
+
+// updateCourse('5f7ff25c5684cb13e84df606').then(()=>{
+//     console.log('course update success');
+// });
+
+// update many will update all with the name mosh 
+// if we use update it will update only one 
+async function updateCourse()
+{
+    // const result = await Course.updateMany({ author: 'Mosh' }, {
+
+        const result = await Course.update({ author: 'Mosh' }, {
+
+        $set: { name: 'Learning hockey'}
+
+
+});
+    console.log('course update success');
 }
 
-updateCourse('5f7ff25c5684cb13e84df606').then(()=>{
-    console.log('course update success');
-});
+updateCourse().then(() =>{
+    console.log('success');
+} );

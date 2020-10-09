d5e7f55350fd9cc2c95a17fa9c4d9921478c7375
Dharani Ganapam <dganapa1@in.ibm.com>
2020-10-09 17:59:12 +0530

    validation for name

--- node-mongo-connection/vidly-ex/routes/vidlyRoute.js
+++ node-mongo-connection/vidly-ex/routes/vidlyRoute.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Joi = require('joi');
 const app = express();
 const router = express.Router();
 const mongoose = require('mongoose');
@@ -6,11 +7,21 @@ const genreSchema = {
     name: String
 }
 const Genre = mongoose.model('genre', genreSchema);
-const genre = new Genre();
+
 router.post('/', async (req, res) => {
-    genre.name = req.body.name.toString();
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    let genre = new Genre({ name: req.body.name });
     let result = await genre.save();
     res.send(result);
 });
+
+function validateGenre(genre) {
+    const schema = {
+        name: Joi.string().min(5).required()
+    };
+
+    return Joi.validate(genre, schema);
+}
 
 module.exports = router;
